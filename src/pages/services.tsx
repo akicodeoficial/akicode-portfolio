4e07bbea1a357bea557233cbde6c29a77f973a5f
Alan Silva <126093767+akicodeoficial@users.noreply.github.com>
2023-05-19 15:45:33 -0300

    Criando páginas

---
+++ src/pages/services.tsx
@@ -0,0 +1,9 @@
+import RootLayout from "../layouts/root";
+
+export default function ServicesPage(): JSX.Element {
+  return (
+    <RootLayout>
+      <h1>Index Page</h1>
+    </RootLayout>
+  );
+}
---
+++ src/pages/social.tsx
@@ -0,0 +1,9 @@
+import RootLayout from "../layouts/root";
+
+export default function SocialPage(): JSX.Element {
+  return (
+    <RootLayout>
+      <h1>Index Page</h1>
+    </RootLayout>
+  );
+}
