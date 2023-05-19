bf3000684ff70eefa6a2fb4e9cc379bfbbf9c1ef
Alan Silva <126093767+akicodeoficial@users.noreply.github.com>
2023-05-19 19:21:09 -0300

    Criando card de projeto

---
+++ src/components/card-project.tsx
@@ -0,0 +1,25 @@
+interface CardProjetProps {
+  date: string;
+  cardDescription: string;
+  imageUrl: string;
+}
+
+export default function CardProjet({
+  date,
+  cardDescription,
+  imageUrl,
+}: CardProjetProps): JSX.Element {
+  return (
+    <div className="col">
+      <div className="card shadow-sm">
+        <img src={imageUrl} />
+        <div className="card-body">
+          <p className="card-text">{cardDescription}</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <small className="text-body-secondary">{date}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,9 +1,37 @@
+import CardProjet from "../components/card-project";
 import RootLayout from "../layouts/root";
+import EraseDatabase from "../assets/erasa-database.png";
 
 export default function ProjectsPage(): JSX.Element {
   return (
     <RootLayout>
-      <h1>Index Page</h1>
+      <header>
+        <div className="p-3">
+          <section className="jumbotron text-center">
+            <div className="container">
+              <h1 className="jumbotron-heading">Meus Projetos</h1>
+              <p className="lead text-muted">
+                Logo Abaixo estão todos os meus projetos do github. estou
+                considerando apenas projetos como sites, aplicações e não
+                configurações ou dotfiles
+              </p>
+            </div>
+          </section>
+        </div>
+      </header>
+      <main>
+        <div className="album py-5 bg-body-tertiary">
+          <div className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <CardProjet
+                date="17/05/2023"
+                imageUrl={EraseDatabase}
+                cardDescription="Um site para um empresa ficticia de vendas de banco de dados"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
     </RootLayout>
   );
 }
