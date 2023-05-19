1d1be57bac9752bd2f999cefe8a0f1e637d4cd00
Alan Silva <126093767+akicodeoficial@users.noreply.github.com>
2023-05-19 19:02:15 -0300

    Criando card inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,27 @@ export default function IndexPage(): JSX.Element {
         <Breadcrumb />
       </header>
       <main>
-        <div className='container'>
-          <div className='row'></div>
+        <div className="container">
+          <div className="row">
+            <div className="card">
+              <div className="card-header">Descrição Curta</div>
+              <div className="card-body">
+                <blockquote className="blockquote mb-0">
+                  <p>
+                    Sou estudante de Ciência da Computação na Universidade
+                    Cruzeiro do Sul e atuo como programador pleno com 3 anos de
+                    experiência.
+                  </p>
+                  <footer className="blockquote-footer">
+                    Escrito em{" "}
+                    <cite title="Source Title">19/05/2023</cite>
+                  </footer>
+                </blockquote>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </RootLayout>
-  )
+  );
 }
