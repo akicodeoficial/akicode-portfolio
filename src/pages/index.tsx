b72a14214754df2163412c75d7dc25953a7dd4de
Alan Silva <126093767+akicodeoficial@users.noreply.github.com>
2023-05-19 19:30:52 -0300

    Criando jornada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
-import Breadcrumb from '../components/breadcrumb';
-import RootLayout from '../layouts/root';
+import RootLayout from "../layouts/root";
 
 export default function IndexPage(): JSX.Element {
   return (
     <RootLayout>
-      <section className="bg-body-secondary">
-        <Breadcrumb />
-      </section>
       <section>
         <div className="container">
-          <div className="row">
+          <div className="row p-3">
             <div className="card">
               <div className="card-header">Descrição Curta</div>
               <div className="card-body">
@@ -20,12 +16,61 @@ export default function IndexPage(): JSX.Element {
                     experiência.
                   </p>
                   <footer className="blockquote-footer">
-                    Escrito em{" "}
-                    <cite title="Source Title">19/05/2023</cite>
+                    Escrito em <cite title="Source Title">19/05/2023</cite>
                   </footer>
                 </blockquote>
               </div>
             </div>
+          </div>
+          <div className="row p-3">
+            <nav id="navbar-example2" className="navbar bg-light px-3 mb-3">
+              <a className="navbar-brand" href="#">
+                Jornada
+              </a>
+              <ul className="nav nav-pills">
+                <li className="nav-item">
+                  <a className="nav-link" href="#scrollspyHeading1">
+                    Primeiro
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#scrollspyHeading2">
+                    Segundo
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    href="#"
+                    role="button"
+                    aria-expanded="false"
+                  >
+                    Outros
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#scrollspyHeading3">
+                        Third
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#scrollspyHeading4">
+                        Fourth
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#scrollspyHeading5">
+                        Fifth
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </section>
