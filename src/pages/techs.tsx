fd35db2857ce72d2a28cdc039e5777d7749f4d39
Alan Silva <126093767+akicodeoficial@users.noreply.github.com>
2023-05-19 17:43:31 -0300

    criando tabela

--- src/pages/techs.tsx
+++ src/pages/techs.tsx
@@ -1,9 +1,29 @@
+import Breadcrumb from '../components/breadcrumb';
 import RootLayout from "../layouts/root";
 
 export default function TechsPage(): JSX.Element {
   return (
     <RootLayout>
-      <h1>Index Page</h1>
+      <header className="bg-body-secondary">
+        <Breadcrumb />
+      </header>
+      <main>
+        <div className="container">
+          <div className="row">
+            <h2>Tecnológias do Node.js</h2>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Link</th>
+                  <th scope="col">Nome</th>
+                  <th scope="col">Uso</th>
+                  <th scope="col">Dominio</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+        </div>
+      </main>
     </RootLayout>
   );
 }
