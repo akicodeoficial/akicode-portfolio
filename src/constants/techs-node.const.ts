559871c02b907d7e04261d63adb1a418b811bc6e
Alan Silva <126093767+akicodeoficial@users.noreply.github.com>
2023-05-19 17:58:23 -0300

    bibliotecas que eu uso no nodejs

---
+++ src/constants/techs-node.const.ts
@@ -0,0 +1,15 @@
+export interface TechsNodeInterface {
+  nome: string;
+  link: string;
+  uso: string;
+  dominio: string;
+}
+
+export const TechsNode: TechsNodeInterface[] = [
+  {
+    nome: "Express.js",
+    link: "https://expressjs.com/pt-br/",
+    uso: "Frequente Com Nestjs",
+    dominio: "Intermediário",
+  },
+];
