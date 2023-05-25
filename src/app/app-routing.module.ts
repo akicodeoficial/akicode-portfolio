164e8676934c3aed698f9a8aad08576fdcdf90d8
Alan Silva <126093767+akicodeoficial@users.noreply.github.com>
2023-05-25 10:47:36 -0300

    Criando página de experiencias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpComponent } from './exp/exp.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'experiencia', component: ExpComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/exp/exp.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ExpComponent } from './exp.component';
+
+@NgModule({
+  declarations: [ExpComponent],
+  imports: [CommonModule],
+})
+export class ExpModule {}
