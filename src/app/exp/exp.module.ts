b04c845c3143ca27c4a5c76cf21feb75a7114ffc
Alan Silva <126093767+akicodeoficial@users.noreply.github.com>
2023-06-02 17:02:08 -0300

    Criando aplicação nextjs

--- src/app/exp/exp.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { ExpComponent } from './exp.component';
-
-@NgModule({
-  declarations: [ExpComponent],
-  imports: [CommonModule],
-})
-export class ExpModule {}
--- src/app/services/services.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { ServicesComponent } from './services.component';
-
-@NgModule({
-  declarations: [ServicesComponent],
-  imports: [CommonModule],
-})
-export class ServicesModule {}
